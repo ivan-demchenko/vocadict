f3a0b28e8b93fd99107725fda0ffec60760f7d9d
Ivan Demchenko <raqy.style@gmail.com>
2013-02-21 22:03:46 +0200

    grunt is power

--- app/Gruntfile.js
+++ app/Gruntfile.js
@@ -5,35 +5,49 @@ module.exports = function (grunt) {
   // Project configuration.
   grunt.initConfig({
 
-    lint: {
-      dist: {
-        files: ['js/app.js']
+    pkg: grunt.file.readJSON('package.json'),
+
+    cssmin: {
+      compress: {
+        files: {
+          "build/app.min.css": ["css/full.css", "css/app.css", "css/theme.css"]
+        }
+      }
+    },
+
+    requirejs: {
+      compile: {
+        options: {
+          name: 'main',
+          optimize: "uglify",
+          baseUrl: "js/",
+          mainConfigFile: "js/config.js",
+          out: "build/app.js"
+        }
+      }
+    },
+
+    uglify: {
+      build: {
+        files: {
+          'build/app.min.js': ['build/app.js']
+        }
       }
     },
 
     jshint: {
-      all: ['Gruntfile.js', 'js/**/*.js']
-    },
-
-    concat: {
-      dist: {
-        src: [
-          'js/app.pretty.js'
-        ],
-        dest: 'build/js/production.js'
-      }
-    },
-
-    min: {
-      dist : {
-        src : 'build/js/production.js',
-        dest : 'build/js/production.min.js'
+      build: {
+        all: ['Gruntfile.js', 'build/app.min.js']
       }
     }
 
   });
 
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks("grunt-contrib-jshint");
-  grunt.registerTask('default', ['jshint']);
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  grunt.registerTask('default', ['cssmin', 'requirejs', 'uglify', 'jshint']);
 
 };
