eb2d3e3a4204d79683acc59c21e1b96ab958c532
Ivan Demchenko <raqy.style@gmail.com>
2013-04-28 16:08:01 +0300

    fix routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,19 +1,13 @@
-
-/*
- * GET home page.
- */
-
 exports.index = function (req, res) {
   var env;
   if (process.env.NODE_ENV !== 'dev' || process.env.NODE_ENV !== 'prod') {
-    env = 'prod'
+    env = 'prod';
   } else {
     env = process.env.NODE_ENV;
   }
   var file = env + '_index';
   res.render(file, { title: 'Vocadict', env: env });
-  }
-};
+}
 
 exports.vkauth = function (req, res) {
   res.render('vkauth');
