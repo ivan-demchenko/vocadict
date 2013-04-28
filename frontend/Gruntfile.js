ca7c72284745c4e42e92255a7a8b6c36c00c45d5
Ivan Demchenko <raqy.style@gmail.com>
2013-04-28 15:40:39 +0300

    temp

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -87,7 +87,7 @@ module.exports = function (grunt) {
 
     exec: {
       clone_cssframework: {
-        command: 'if [ ! -d "components/cssframework" ]; then cd components/ && git clone https://github.com/raqystyle/cssframework.git fi'
+        command: 'if [ ! -d "components/cssframework" ]; then cd components/ && git clone https://github.com/raqystyle/cssframework.git; fi'
       }
     },
 
